3604847f5eaa6ab1e77f67bf02576859d116f154
Carla Aldana <carliss.aldana@gmail.com>
2023-02-26 23:02:25 -0300

    cambios grales

---
+++ Proyecto Scarlatta/JS/main.js
@@ -0,0 +1,13 @@
+window.sr = ScorllReveal();
+
+    sr.reveal('.navbar', {
+        duration: 3000,
+        origin:'bottom',
+        distance:'-100px'
+    });
+
+    sr.reveal('.main-text', {
+        duration: 3000,
+        origin:'bottom',
+        distance:'-100px'
+    });
